6fb96dac7f919a684b1597de0706350fd87b4610
Mosaab <musab.ehab@gmail.com>
2022-11-19 01:35:19 +0200

    add elastic search to message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,35 @@
 class Message < ApplicationRecord
   belongs_to :chat
+
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  mapping do
+    indexes :body, type: :text
+    indexes :chat_id, type: :integer
+  end
+
+  def self.search(query, chat_id = nil)
+    params = {
+      query: {
+        bool: {
+          must: [
+            {
+              multi_match: {
+                query: query,
+                fields: [:body]
+              }
+            },
+          ],
+          filter: [
+            {
+              term: { chat_id: chat_id }
+            }
+          ]
+        }
+      }
+    }
+
+    self.__elasticsearch__.search(params)
+  end
 end
