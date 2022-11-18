0df8fe8c9e4b04c301091f9767f5a3df25d2ff8c
Mosaab <musab.ehab@gmail.com>
2022-11-19 01:36:13 +0200

    schema.rb changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_231949) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_18_014206) do
   create_table "applications", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "token", null: false
@@ -30,5 +30,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_231949) do
     t.index ["application_id"], name: "index_chats_on_application_id"
   end
 
+  create_table "messages", charset: "utf8mb4", force: :cascade do |t|
+    t.integer "message_number", default: 0, null: false
+    t.text "body", null: false
+    t.bigint "chat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
+  end
+
   add_foreign_key "chats", "applications"
+  add_foreign_key "messages", "chats"
 end
