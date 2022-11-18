9fb55bf53e6f74b473fcfa00a67b5219c8c6e632
Mosaab <musab.ehab@gmail.com>
2022-11-18 18:40:48 +0200

    add new helpers for response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,41 @@
 class ApplicationController < ActionController::API
   def not_found_response
-    render json: { meta: { status: false, message: "Not Found" } }, status: :not_found
+    render json: {
+      status: false,
+      message: "Not Found",
+      data: {},
+      meta: {}
+    }, status: :not_found
   end
 
   def validation_error(field)
     render json: {
-      error: field + " can not be empty" ,
-      meta: {
-        status: false ,
-        message: "Validation Error"
-      }
+      status: false,
+      message: "Validation Error",
+      data: {
+        error: field + " can not be empty",
+      },
+      meta:{}
     }, status: :unprocessable_entity
+  end
+
+  def error_response(error)
+    render json: {
+      status: false,
+      message: error,
+      data: {
+        error: error,
+      },
+      meta:{}
+    }, status: :unprocessable_entity
+  end
+
+  def success_response(data, meta = {}, code = 200, message = "Success", status = true)
+    render json: {
+      status: status,
+      message: message,
+      data: data,
+      meta: meta
+    }, status: code
   end
 end
