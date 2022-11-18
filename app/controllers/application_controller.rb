aa44bf4940a70f5b3529d80d1b75615aeb8a1337
Mosaab <musab.ehab@gmail.com>
2022-11-18 17:53:21 +0200

    move error and not_found response to base controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  def not_found_response
+    render json: { meta: { status: false, message: "Not Found" } }, status: :not_found
+  end
+
+  def validation_error(field)
+    render json: {
+      error: field + " can not be empty" ,
+      meta: {
+        status: false ,
+        message: "Validation Error"
+      }
+    }, status: :unprocessable_entity
+  end
 end
