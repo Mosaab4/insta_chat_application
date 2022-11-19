09d9433ef2fb0be7c8b74c227e451c395ac1cabc
Mosaab <musab.ehab@gmail.com>
2022-11-20 00:40:45 +0200

    update jobs to store data to redis

--- app/sidekiq/update_application_chats_count_job.rb
+++ app/sidekiq/update_application_chats_count_job.rb
@@ -4,5 +4,11 @@ class UpdateApplicationChatsCountJob
   def perform(application_id)
     count = Chat.where(application_id: application_id).count
     Application.where(id: application_id).update(chats_count: count)
+
+    application = Application.find(application_id)
+    $redis.set(Application.redis_key(application.token), application.to_json)
+
+    chats = Chat.where(application_id: application).to_json
+    $redis.set(Application.chats_redis_key(application.token), chats)
   end
 end
--- app/sidekiq/update_chat_messages_count_job.rb
+++ app/sidekiq/update_chat_messages_count_job.rb
@@ -4,5 +4,11 @@ class UpdateChatMessagesCountJob
   def perform(chat_id)
     count = Message.where(chat_id: chat_id).count
     Chat.where(id: chat_id).update(messages_count: count)
+
+    chat = Chat.find(chat_id)
+    $redis.set(Chat.redis_key(chat.id), chat.to_json)
+
+    messages = Message.where(chat_id: chat_id).to_json
+    $redis.set(Chat.messages_redis_key(chat_id), messages)
   end
 end
