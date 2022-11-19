bc907103819c2af2dd0c14b6be1ede3595d54fdc
Mosaab <musab.ehab@gmail.com>
2022-11-20 00:39:44 +0200

    add methods to access redis key in models

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,12 @@ class Application < ApplicationRecord
   validates_uniqueness_of :name
 
   has_many :chats
+
+  def self.redis_key(token)
+    "application_data_" + token
+  end
+
+  def self.chats_redis_key(token)
+    "application_chats_" + token
+  end
 end
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,12 @@
 class Chat < ApplicationRecord
   belongs_to :application
   has_many :messages
+
+  def self.redis_key(id)
+    "chat_data_" + id.to_s
+  end
+
+  def self.messages_redis_key(id)
+    "chat_messages_" + id.to_s
+  end
 end
