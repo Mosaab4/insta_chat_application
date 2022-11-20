bf9b6262e27e073716305941ff693a304b19551f
Mosaab <musab.ehab@gmail.com>
2022-11-20 06:49:58 +0200

    update the job UpdateChatMessagesCountJob to use application_token in caching

--- app/sidekiq/update_chat_messages_count_job.rb
+++ app/sidekiq/update_chat_messages_count_job.rb
@@ -6,13 +6,13 @@ class UpdateChatMessagesCountJob
     Chat.where(id: chat_id).update(messages_count: count)
 
     chat = Chat.find(chat_id)
-    $redis.set(Chat.redis_key(chat.id), chat.to_json)
-
     application = chat.application
+    $redis.set(Chat.redis_key(chat.id, application.token), chat.to_json)
+
     chats = application.chats.to_json
     $redis.set(Application.chats_redis_key(application.token), chats)
 
     messages = Message.where(chat_id: chat_id).to_json
-    $redis.set(Chat.messages_redis_key(chat_id), messages)
+    $redis.set(Chat.messages_redis_key(chat.chat_number, application.token), messages)
   end
 end
