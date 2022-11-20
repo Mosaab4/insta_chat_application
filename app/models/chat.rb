af27de6d3a90156357997ad2c2234dee873d81f4
Mosaab <musab.ehab@gmail.com>
2022-11-20 06:48:48 +0200

    add application_token to redis key for chat

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,11 +2,11 @@ class Chat < ApplicationRecord
   belongs_to :application
   has_many :messages
 
-  def self.redis_key(id)
-    "chat_data_" + id.to_s
+  def self.redis_key(chat_number,application_token)
+    "chat_data_" + chat_number.to_s + '_' + application_token
   end
 
-  def self.messages_redis_key(id)
-    "chat_messages_" + id.to_s
+  def self.messages_redis_key(chat_number ,application_token)
+    "chat_messages_" + chat_number.to_s + '_' + application_token
   end
 end
