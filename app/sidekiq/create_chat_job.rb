d94efea749838dd523e6b6d4de450a3b473395ec
Mosaab <musab.ehab@gmail.com>
2022-11-21 14:25:29 +0200

    update jobs to handle multiple insters

--- app/sidekiq/create_chat_job.rb
+++ app/sidekiq/create_chat_job.rb
@@ -4,14 +4,18 @@ class CreateChatJob
   def perform(application_id,application_token)
     chat_number = 0
 
-    last_chat_entry = Chat.where(application_id: application_id).order('id DESC').first
+    ActiveRecord::Base.transaction do
+      ActiveRecord::Base.with_advisory_lock("add_chat_#{application_token}_lock") do
+        last_chat_entry = Chat.where(application_id: application_id).order('id DESC').first
 
-    unless last_chat_entry.nil?
-      chat_number = last_chat_entry.chat_number
+        unless last_chat_entry.nil?
+          chat_number = last_chat_entry.chat_number
+        end
+
+        Chat.create(application_id: application_id, chat_number: chat_number + 1)
+
+        $redis.del(Application.chats_redis_key(application_token))
+      end
     end
-
-    Chat.create(application_id: application_id, chat_number: chat_number + 1)
-
-    $redis.del(Application.chats_redis_key(application_token))
   end
 end
--- app/sidekiq/create_message_job.rb
+++ app/sidekiq/create_message_job.rb
@@ -4,15 +4,18 @@ class CreateMessageJob
   def perform(chat_id, body, chat_number,application_token)
     message_number = 0
 
-    last_message_entry = Message.where(chat_id: chat_id).order('id DESC').first
+    ActiveRecord::Base.transaction do
+      ActiveRecord::Base.with_advisory_lock("add_message_#{chat_number.to_s}_#{application_token}_lock") do
+        last_message_entry = Message.where(chat_id: chat_id).order('id DESC').first
 
-    unless last_message_entry.nil?
-      message_number = last_message_entry.message_number
+        unless last_message_entry.nil?
+          message_number = last_message_entry.message_number
+        end
+
+        Message.create(body: body, chat_id: chat_id, message_number: message_number + 1)
+
+        $redis.del(Chat.messages_redis_key(chat_number,application_token))
+      end
     end
-
-    Message.create(body: body, chat_id: chat_id, message_number: message_number + 1)
-
-    $redis.del(Chat.messages_redis_key(chat_number,application_token))
-
   end
 end
